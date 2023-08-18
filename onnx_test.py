e2686b75b6fe215e1c338b9cff07a4f22ae177b3
yoosk526 <yoosk526@naver.com>
2023-08-18 23:02:08 +0900

    Update

--- onnx_test.py
+++ onnx_test.py
@@ -9,10 +9,10 @@ def bicubicResize(x:np.ndarray, scale:int=4):
 	x = cv2.resize(x, dsize=(w*scale, h*scale), interpolation=cv2.INTER_NEAREST)
 	return x
 
-lrOrig = openImage("./media/ar_02.png")
+lrOrig = openImage("./media/aerial_ku.png")
 lrObj = np.transpose(lrOrig, [2, 0, 1])
 lrObj = np.expand_dims(lrObj, axis=0).astype(np.float32) / 255.0
-ort_sess = ort.InferenceSession("./model/x4_224_320.onnx", providers=['CUDAExecutionProvider'])
+ort_sess = ort.InferenceSession("./model/x4_270_480.onnx", providers=['CUDAExecutionProvider'])
 outputs = (ort_sess.run(None, {'input': lrObj})[0] * 255.0).astype(np.uint8)
 srObj = np.squeeze(outputs, axis=0)
 srObj = np.transpose(srObj, [1, 2, 0])
@@ -21,10 +21,10 @@ srObj = cv2.cvtColor(srObj, cv2.COLOR_RGB2BGR)
 # cv2.imwrite("result.png", srObj)
     
 bicubic = bicubicResize(lrOrig)
-total_img = np.hstack((bicubic, srObj))
+# total_img = np.hstack((bicubic, srObj))
 
-# cv2.imwrite("./media/x4_bicubic_02.png", bicubic)
-cv2.imwrite("./media/x4_abpn_02.png", srObj)
+cv2.imwrite("./media/result/x4_bicubic_ku.png", bicubic)
+cv2.imwrite("./media/result/x4_abpn_ku.png", srObj)
 
 '''
 while True:
--- trt_test.py
+++ trt_test.py
@@ -4,13 +4,13 @@ from libs.utils import *
 
 if __name__ == "__main__":
     trt_model = edgeSR_TRT_Engine(
-        engine_path="./model/x4_224_320.trt", scale=4, lr_size=(224,320)
+        engine_path="./model/x4_270_480.trt", scale=4, lr_size=(270, 480)
     )
-    lrOrig = openImage("./media/ar_02.png")
+    lrOrig = openImage("./media/aerial_ku.png")
     lrObj = np.transpose(lrOrig, [2, 0, 1])
     lrObj = np.ascontiguousarray(lrObj, dtype=np.float32)
     while True:
-        cv2.imshow("trt_img", lrOrig)
+        cv2.imshow("Low resolution", lrOrig)
         key = cv2.waitKey(1)
         if key == 27:
             break
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     srObj = cv2.cvtColor(srObj, cv2.COLOR_RGB2BGR)
     
     while True:
-        cv2.imshow("trt_img", srObj)
+        cv2.imshow("High resolution", srObj)
         key = cv2.waitKey(1)
         if key == 27:
             break
