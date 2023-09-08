70df741dd27f0f9836d0f5336c7364e455d5fd77
yoosk526 <yoosk526@naver.com>
2023-09-09 00:17:52 +0900

    Bicubic resize code

---
+++ bicubic_resize.py
@@ -0,0 +1,25 @@
+import argparse
+import numpy as np
+import cv2
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+	"--img_dir", type=str, default="./media/ori/270_480_01.png"
+)
+parser.add_argument(
+	"--save_dir", type=str, default="./media/ref/x4_270_480_bicubic_01.png"
+)
+
+def bicubicResize(x:np.ndarray, scale:int=4):
+	h, w, _ = x.shape
+	x = cv2.cvtColor(x, cv2.COLOR_RGB2BGR)
+	x = cv2.resize(x, dsize=(w*scale, h*scale), interpolation=cv2.INTER_NEAREST)
+	return x
+
+if __name__ == "__main__":
+	args = parser.parse_args()
+	
+	lrOrig = cv2.imread(args.img_dir)
+	bicubic = bicubicResize(lrOrig)
+	
+	cv2.imwrite(args.save_dir, bicubic)
