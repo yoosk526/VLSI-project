8cf2a4a51a20d589e92881a4cc000ca659ad5b45
yoosk526 <yoosk526@naver.com>
2023-10-16 22:02:39 +0900

    Modify code

--- bicubic_resize.py
+++ bicubic_resize.py
@@ -9,8 +9,11 @@ parser.add_argument(
 parser.add_argument(
 	"--save_dir", type=str, default="./media/ref/x4_270_480_bicubic_01.png"
 )
+parser.add_argument(
+	"--scale", type=int, default=4
+)
 
-def bicubicResize(x:np.ndarray, scale:int=4):
+def bicubicResize(x:np.ndarray, scale:int):
 	h, w, _ = x.shape
 	x = cv2.resize(x, dsize=(w*scale, h*scale), interpolation=cv2.INTER_NEAREST)
 	return x
@@ -19,6 +22,6 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 	
 	lrOrig = cv2.imread(args.img_dir)
-	bicubic = bicubicResize(lrOrig)
+	bicubic = bicubicResize(lrOrig, args.scale)
 	
 	cv2.imwrite(args.save_dir, bicubic)
