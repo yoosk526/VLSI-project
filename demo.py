2c2afb69bcabc82addcccb73c93c3863f52796e6
yoosk526 <yoosk526@naver.com>
2023-10-22 21:24:22 +0900

    Modify code in terms of normalization

--- demo.py
+++ demo.py
@@ -23,32 +23,9 @@ parser.add_argument(
 parser.add_argument(
     "--scale", type=int, default=4
 )
-
-def preprocess(x:np.ndarray):
-    x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
-    x = np.transpose(x, [2, 0, 1])      # H, W, C -> C, H, W
-    x = x.astype(np.float32) / 255.0    # Normalization
-    x = np.ascontiguousarray(x, dtype=np.float32)
-    return x
-
-def postprocess(x:np.ndarray):
-    x = (x * 255.0).astype(np.uint8)
-    x = np.transpose(x, [1, 2, 0])      # C, H, W -> H, W, C
-    x = cv2.cvtColor(x, cv2.COLOR_RGB2BGR)
-    return x    
-
-def bicubicResize(x:np.ndarray, scale:int):
-    h, w, _ = x.shape
-    x = cv2.resize(x, dsize=(w*scale, h*scale), interpolation=cv2.INTER_NEAREST)
-    return x
-
-def horizontalFusion(bi:np.ndarray, sr:np.ndarray):
-    assert bi.shape == sr.shape
-    h, w, c = bi.shape
-    canvas = np.zeros_like(bi).astype(np.uint8)
-    canvas[:, 0:w//2, :] = bi[:, 0+500:w//2+500, :]
-    canvas[:, w//2:w, :] = sr[:, 0+500:w//2+500, :]
-    return canvas
+parser.add_argument(
+	"--norm", action="store_true"
+)
 
 if __name__ == "__main__":
     opt = parser.parse_args()
@@ -81,8 +58,8 @@ if __name__ == "__main__":
         if not ret:
             break
         bicubic = bicubicResize(frame, upscale)
-        input_np = preprocess(frame)
-        sr_np = postprocess(trt_model(input_np))
+        input_np = preprocess(frame, opt.norm)
+        sr_np = postprocess(trt_model(input_np), opt.norm)
         key = cv2.waitKey(frameRate)
         if key == 27:
             break
