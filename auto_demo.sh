59af175f4c880e5575e107e72b95bcc85e2b9919
yoosk526 <yoosk526@naver.com>
2023-11-12 21:18:10 +0900

    Auto demo

--- auto_demo.sh
+++ auto_demo.sh
@@ -2,12 +2,5 @@ while :
 do
     python trt_test.py 
     python demo.py 
-
-    read -n 1 -s -t 1 input
-
-    if [[ $input = $'e' ]]; then
-        break
-    fi
 done
 
-echo "ESC key pressed to end script"
