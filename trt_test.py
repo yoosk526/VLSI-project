7bd712127bc3594703820b19840c61aeacfe3bf1
yoosk526 <yoosk526@naver.com>
2023-11-12 21:22:57 +0900

    Test code for image using TensorRT

--- trt_test.py
+++ trt_test.py
@@ -43,10 +43,10 @@ if __name__ == "__main__":
 	canvas = horizontalFusion(biObj, srObj)
 
 	BICUBIC_SR_WINDOW = "BICUBIC vs SUPER-RESOLUTION"
-    cv2.namedWindow(BICUBIC_SR_WINDOW)
-    cv2.moveWindow(BICUBIC_SR_WINDOW, 570, 250)
-    cv2.imshow(BICUBIC_SR_WINDOW, canvas)
-    cv2.waitKey(5000)
+	cv2.namedWindow(BICUBIC_SR_WINDOW)
+	cv2.moveWindow(BICUBIC_SR_WINDOW, 570, 250)
+	cv2.imshow(BICUBIC_SR_WINDOW, canvas)
+	cv2.waitKey(5000)
 	cv2.destroyAllWindows()
 
 	'''
